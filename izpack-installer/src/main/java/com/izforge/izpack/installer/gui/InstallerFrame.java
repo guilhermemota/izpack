b291a498eff41e25eabb89653079f94be515d371
Cristiano Neves <cristiano.neves@datacom.ind.br>
2014-01-13 14:18:45 -0200

    Ajuste layout (linha acima dos botões previous/next/quit).
    Pequeno ajuste no layout: remoção do caractere espaço "hard coded" após o valor da chave
    "installer.madewith" no text usado junto a linha inferior (acima dos botões).
    
    Com este espaço eliminado, é possível redefinr o valor da chave "installer.madewith" no
    arquivo de mensagens customizado e utilizar uma String em branco, sem impacto no layout
    (o caractere de espaço existente gerava um espaçamento indesejado nessas situações).
    
    Este ajuste foi necessário para NÃO exibir o nome do produto IzPack nas telas do instalador.
    Verificou-se não ser necessário exibir nenhuma informação neste lugar, por isso foi necessário
    eliminar o caractere espaço.
    
    Workaround:
    
    --- a/izpack-installer/src/main/java/com/izforge/izpack/installer/gui/InstallerFrame.java
    +++ b/izpack-installer/src/main/java/com/izforge/izpack/installer/gui/InstallerFrame.java
    @@ -339,7 +339,7 @@ public class InstallerFrame extends JFrame implements InstallerView
             JPanel navPanel = new JPanel();
             navPanel.setLayout(new BoxLayout(navPanel, BoxLayout.X_AXIS));
             TitledBorder border = BorderFactory.createTitledBorder(
    -                new EtchedLineBorder(), messages.get("installer.madewith") + " ",
    +                new EtchedLineBorder(), messages.get("installer.madewith"),
                     TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, new Font("Dialog", Font.PLAIN, 10));
             navPanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(8, 8, 8, 8), border));

--- izpack-installer/src/main/java/com/izforge/izpack/installer/gui/InstallerFrame.java
+++ izpack-installer/src/main/java/com/izforge/izpack/installer/gui/InstallerFrame.java
@@ -339,7 +339,7 @@ public class InstallerFrame extends JFrame implements InstallerView
         JPanel navPanel = new JPanel();
         navPanel.setLayout(new BoxLayout(navPanel, BoxLayout.X_AXIS));
         TitledBorder border = BorderFactory.createTitledBorder(
-                new EtchedLineBorder(), messages.get("installer.madewith") + " ",
+                new EtchedLineBorder(), messages.get("installer.madewith"),
                 TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, new Font("Dialog", Font.PLAIN, 10));
         navPanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(8, 8, 8, 8), border));
 
